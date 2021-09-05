2d5fc9976084fb855ac6aca50443c8ea780794d5
Crashcrafter <n.hack2810@gmail.com>
2021-09-05 13:15:23 +0200

    added maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.30"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    `maven-publish`
 }
 
 group = "dev.crash"
@@ -13,10 +14,22 @@ repositories {
     mavenLocal()
 }
 
+apply(plugin = "maven-publish")
+
 dependencies {
     implementation("io.ktor:ktor-server-core:1.6.3")
     implementation("io.ktor:ktor-server-netty:1.6.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.30")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven"){
+            groupId = "dev.crash"
+            artifactId = "kotlinp2p"
+            version = "0.1"
+        }
+    }
 }
 
 tasks.test {
@@ -25,4 +38,10 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "16"
+}
+
+tasks.withType<PublishToMavenLocal> {
+    doLast {
+        project.file("/build/libs/${project.name}-$version-all.jar").copyTo(file("C:\\Users\\${System.getProperties()["user.name"]}\\.m2\\repository\\dev\\crash\\kotlinp2p\\0.1\\${project.name}-$version-all.jar"), overwrite = true)
+    }
 }
